8b757b92334133ede87dc42300a9bb608cfc8765
OBrooks <mr.willh@gmail.com>
2018-11-26 11:08:47 +0100

    templates + en forme + db items

---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+    validates :name, presence: true
+    validates :name, presence: true
+    validates :name, presence: true
+    validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_093521) do
+ActiveRecord::Schema.define(version: 2018_11_26_095748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
