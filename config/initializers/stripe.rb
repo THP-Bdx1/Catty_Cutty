dc3310295bb6e0c45a665265df99c128c21662fc
OBrooks <mr.willh@gmail.com>
2018-11-29 17:53:16 +0100

    léclémaggle

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 require 'dotenv'
 
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => Rails.application.credentials.dig(:PUBLISHABLE_KEY),
+  :secret_key      => Rails.application.credentials.dig(:SECRET_KEY)
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
